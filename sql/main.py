d5e8a912c919d5fbee4005217a24063deb82dfb3
Rodrigo Assis <135379745+RodrigoAssis7@users.noreply.github.com>
2025-06-26 22:13:25 -0300

    primeiro comit

---
+++ sql/main.py
@@ -0,0 +1,12 @@
+import sqlite3
+from tkinter import messagebox
+
+class SistemaDeRegistro:
+    def __init__(self):
+        self.com = sqlite3.connect('estudante.db')
+        self.c = self.com.cursor()
+        self.creat_table()
+        
+    def creat_table(self):
+        self.c.execute()
+        
