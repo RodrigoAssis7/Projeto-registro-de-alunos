077c1ad335b7d7ba4cc252dedc0ea4621a1b9a64
Rodrigo Assis <135379745+RodrigoAssis7@users.noreply.github.com>
2025-07-01 21:22:50 -0300

    2

--- SQLlit/main.py
+++ SQLlit/main.py
@@ -3,8 +3,8 @@ from tkinter import messagebox
 
 class SistemaDeRegistro:
     def __init__(self):
-        self.com = sqlite3.connect('estudante.db')
-        self.c = self.com.cursor()
+        self.conn = sqlite3.connect('estudante.db')
+        self.c = self.conn.cursor()
         self.create_table() 
         
     def create_table(self): 
@@ -19,50 +19,52 @@ class SistemaDeRegistro:
                 curso TEXT NOT NULL,
                 picture TEXT NOT NULL) ''')
         
-        def register_estudent(self, estudantes):
-            self.c.execute("INSERT INTO estudantes(nome, email, tel, sexo, data_nascimento, endereco, curso, picture) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+    def register_estudent(self, estudantes):
+      self.c.execute("INSERT INTO estudantes(nome, email, tel, sexo, data_nascimento, endereco, curso, picture) VALUES (?,?,?,?,?,?,?,?)",
                            (estudantes))
-        self.com.commit()
+      self.conn.commit()
 
-        #mostrando mensagem de suscesso
-        messagebox.showinfo('sucesso', 'Registro com sucesso!')
+      #mostrando mensagem de suscesso
+      messagebox.showinfo('sucesso', 'Registro com sucesso!')
         
-        def view_all_students(self):
-            self.c.execut("SELECT * FROM estudantes")
-            dados = self.c.fetchall()
+    def view_all_students(self):
+      self.c.execute("SELECT * FROM estudantes")
+      dados = self.c.fetchall()
 
-            for i in dados:
-                print(f'ID : {i[0]} | Nome : {i[1]} | Email: {i[2]} | Tel: {i[3]} | Sexo: {i[4]} | Data de nascimento: {i[5]} | Endereco: {i[6]} | Curso: {i[7]} | Picture: {i[8]}')
-        def search_student(self, id):
-            self.c.execut("SELECT * FROM estudantes WHERE id=?", (id))
-            dados = self.c.fetchone()
+      for i in dados:
+           print(f'ID : {i[0]} | Nome : {i[1]} | Email: {i[2]} | Tel: {i[3]} | Sexo: {i[4]} | Data de nascimento: {i[5]} | Endereco: {i[6]} | Curso: {i[7]} | Picture: {i[8]}')
+
+    def search_student(self, id):
+     self.c.execut("SELECT * FROM estudantes WHERE id=?", (id))
+     dados = self.c.fetchone()
             
-            print(f'ID : {dados[0]} | Nome : {dados[1]} | Email: {dados[2]} | Tel: {dados[3]} | Sexo: {dados[4]} | Data de nascimento: {dados[5]} | Endereco: {dados[6]} | Curso: {dados[7]} | Picture: {dados[8]}')
+     print(f'ID : {dados[0]} | Nome : {dados[1]} | Email: {dados[2]} | Tel: {dados[3]} | Sexo: {dados[4]} | Data de nascimento: {dados[5]} | Endereco: {dados[6]} | Curso: {dados[7]} | Picture: {dados[8]}')
 
-        def update_students(self, novo_valores):
-            query = "UPDATE estudantes SET nome=?, email=?. tel=?, sexo=?, data_nascimento=?, curso=?, picture=? WERW id=?"
-            self.c.execute(query, novo_valores)
-        self.com.commit()
+
+    def update_students(self, nova_valores):
+        query = "UPDATE estudantes SET nome=?, email=?, tel=?, sexo=?, data_nascimento=?, curso=?, picture=? WERW id=?"
+        self.c.execute(query, nova_valores)
+        self.conn.commit()
 
         #mostrando mensagem de suscesso
-        messagebox.showinfo('sucesso', f'Estudante com ID{novo_valores[8]} Foi atualizado!')
+        messagebox.showinfo('sucesso', f'Estudante com ID:{nova_valores[8]} Foi atualizado!')
 
-        def delete_student(self, id):
-            self.c.execute("DELETE FROM estudantes WHERE id=?", (id,))
-        self.com.commit()
+    def delete_student(self, id):
+        self.c.execute("DELETE FROM estudantes WHERE id=?", (id,))
+        self.conn.commit()
 
           #mostrando mensagem de suscesso
-        messagebox.showinfo('sucesso', f'Estudante com ID{id} Foi Deletado!')
+        messagebox.showinfo('sucesso', f'Estudante com ID:{id} Foi Deletado!')
     
 #Criando uma instancia do sistema de registro
-        sistema_de_registro = SistemaDeRegistro()
+sistema_de_registro = SistemaDeRegistro()
 
 #informacoes
 #estudante = ('joao', 'joao@gmail.com', '1234', 'M', '01/05/2005', 'Belo horizonte', 'informatica', "imagem.png")
 
 #sistema_de_registro.register_estudent(estudante)
 
-# Ver estudantes 
+# Ver estudantes
 #todos_alunos = sistema_de_registro.view_all_students()
 
 # procurar aluno
